d4fd7929c6265718be7c04c4fb96bc210fc02330
Albert Tran <alberdt2@uci.edu>
2021-01-13 23:12:45 -0800

    my changes

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Albert Tran on 1/13/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
